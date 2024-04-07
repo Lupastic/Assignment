b070e89b269c1aa3870db2963a544d114681856f
assignment1 <begimkulovbaktybke1@gmail.com>
2024-04-07 23:01:25 +0500

    3 task

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,48 @@
 public class Main {
-    public static int find_minimum(int[] m, int index) {
-        if (index == m.length - 1) {
-            return m[index];
+    public static int find_minimum(int[] m, int i) {
+        if (i == m.length - 1) {
+            return m[i];
         }
-        return Math.min(m[index], find_minimum(m, index + 1));
+        return Math.min(m[i], find_minimum(m, i + 1));
     }
-    public static double find_average(int[] m, int index, int sum) {
-        if (index == m.length) {
+    public static double find_average(int[] m, int i, int sum) {
+        if (i == m.length) {
             return (double) sum / m.length;
         }
 
-        return find_average(m, index + 1, sum + m[index]);
+        return find_average(m, i + 1, sum + m[i]);
     }
-    public static boolean is_prime(int n){
-        if (n%2==1){
-            return true;
-        }
-        else {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
             return false;
+        } else if (n <= 3) {
+            return true;
+        } else if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        } else {
+            return isPrimeHelper(n, 5);
         }
     }
+
+        private static boolean isPrimeHelper(int n, int divisor) {
+            if (divisor * divisor > n) {
+                return true;
+            } else if (n % divisor == 0) {
+                return false;
+            } else {
+                return isPrimeHelper(n, divisor + 2);
+            }
+        }
+
     public static void main(String[] args) {
         int[] m = {10,1,32,3,45};
         int min = find_minimum(m, 0);
         double avg = find_average(m, 0, 0);
         System.out.println("1 task " + min);
         System.out.println("2 task" + avg);
-
+        int number = 11;
+        boolean isPrime = isPrime(number);
+        System.out.print("3 task");
+        System.out.println(isPrime ? "Prime" : "Composite");
     }
 }
